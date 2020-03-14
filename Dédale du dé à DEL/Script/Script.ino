e71c676284ffdc33c2e0ac771c17e18cc087c208
unknown <etienne-2.beauchamp@polymtl.ca>
2020-03-14 11:01:09 -0400

    Dédale du dé à DEL terminée et fonctionnelle

--- Dédale du dé à DEL/Script/Script.ino
+++ Dédale du dé à DEL/Script/Script.ino
@@ -1,68 +1,35 @@
-// Basic script from: http://www.circuitbasics.com/arduino-7-segment-display-tutorial/
+// Circuit et script inspiré de: http://www.circuitbasics.com/arduino-7-segment-display-tutorial/
 
 #include "SevSeg.h"
 SevSeg sevseg;
 
-const int  buttonPin = 10;    // the pin that the pushbutton is attached to
-int buttonState = 0;          // current state of the button
-int lastButtonState = LOW;    // previous state of the button
-int buttonPushCounter = 0;    // counter for the number of button presses
-long counter = 0;
-long max_long_val = 2147483647L;
+const int buttonPin = 10;  // Broche connectée au bouton poussoir
+int buttonState = LOW;     // État du bouton
+short diceRoll = 1;        //
 
 void setup(){
-  byte numDigits = 1;
-  byte digitPins[] = {};
-  byte segmentPins[] = {3, 2, 7, 9, 8, 4, 5, 6}; // A, B, C, D, E, F, G, CP      G, F, A, B & E, D, C, CP
-  bool resistorsOnSegments = true; 
-
-  byte hardwareConfig = COMMON_CATHODE;
+  // Configuration pour l'afficheur 7 segments 
+  byte numDigits = 1; // Un seul chiffre peut être affiché
+  byte digitPins[] = {}; // Aucune assignation requise puisqu'il n'y a qu'une seul chiffre
+  byte segmentPins[] = {3, 2, 7, 9, 8, 4, 5, 6}; // Correspond respectivement aux broches A, B, C, D, E, F, G, CP
+  bool resistorsOnSegments = true; // Le circuit comprend une résistance
+  byte hardwareConfig = COMMON_CATHODE; // Les afficheurs utilisés sont à cathode commune
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
-  sevseg.setBrightness(90);
+  sevseg.setBrightness(100); // Luminosité de l'affichage
 
-  pinMode(buttonPin, INPUT_PULLUP);
-  Serial.begin(9600);
-  lastButtonState = LOW;
+  // Configuration du bouton poussoir
+  pinMode(buttonPin, INPUT_PULLUP); // Bouton mis en mode pull-up
+  Serial.begin(9600); // Vitesse de transmission des données (baud)
 }
 
 void loop(){
-  buttonState = digitalRead(buttonPin);
-  if(buttonState == HIGH){
-    buttonState = LOW;
-    }
+  buttonState = digitalRead(buttonPin); // Lecture de l'état du boutton
 
-  else
-   buttonState = HIGH;
-
-  if(buttonState == HIGH){
-     Serial.println("on");
-     lastButtonState = HIGH;
-     buttonPushCounter++;
-     if(counter < max_long_val)
-       counter++;
-     buttonPushCounter %= 9;
-     sevseg.setNumber(buttonPushCounter, 1);    
-     sevseg.refreshDisplay();     
-     delay(100 - (counter%99));
-     }
-  
-  else{
-     Serial.println("off");
-     if(lastButtonState == HIGH){
-       Serial.println("in");
-       buttonPushCounter++;
-       buttonPushCounter %= 7;
-       if(buttonPushCounter == 0)
-         buttonPushCounter = 1;
-       counter--;
-       sevseg.setNumber(buttonPushCounter, 1);    
-       sevseg.refreshDisplay();
-       delay(100 - (counter%99));
-       if(counter == 0){
-         lastButtonState = LOW;
-         sevseg.setNumber(buttonPushCounter, 0);
-         sevseg.refreshDisplay();         
-         }
-       }
-     }
+  // Cas où le bouton est enfoncé
+  if(buttonState == LOW){
+    diceRoll = random(1, 7); // Générer un nombre entier aléatoire entre 1 et 6
+    sevseg.setNumber(diceRoll, 1); // Modifier le nombre à afficher 
+    sevseg.refreshDisplay(); // Rafraîchissement de l'afficheur
+    delay(random(20, 60)); // Ajout d'un délai aléatoire (entre 20 et 59 ms)
+  }
 }
